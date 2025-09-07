9732d928486cc942002cee4571df5c0a5df94279
iplitharas <johnplitharas@gmail.com>
2025-09-07 20:55:35 +0300

    add list method in flagship

--- src/services/flagship_srv.py
+++ src/services/flagship_srv.py
@@ -3,14 +3,25 @@ from uuid import UUID
 
 
 class FlagShipRepo:
-    def store(self):
-        pass
+    def __init__(self) -> None:
+        self.store: dict[UUID, Flag] = {}
+
+    def save(self, flag: Flag) -> Flag:
+        self.store[flag.id] = flag
+        return flag
 
 
 class FlagShipService:
-    def __init__(self, repo) -> None:
-        pass
+    def __init__(self, repo: FlagShipRepo | None = None) -> None:
+        self.repo = repo or FlagShipRepo()
 
-    def add(self, name: str, value: bool, desc: str | None = None) -> UUID:
+    def add(self, name: str, value: bool, desc: str | None = None) -> Flag:
+        """
+        Users can `add` new `Flags` to their `store` by `name` and `value`
+        """
         new_flag = Flag(name=name, value=value, desc=desc)
-        return new_flag.id
+        self.repo.save(flag=new_flag)
+        return new_flag
+
+    def list(self) -> list[Flag]:
+        return list(self.repo.store.values())
--- tests/test_flagship_srv.py
+++ tests/test_flagship_srv.py
@@ -1,17 +1,47 @@
 from src.services.flagship_srv import FlagShipService
+from uuid import UUID
 
 
 def test_user_can_add_a_new_flag():
     """
-    Given a `flag` name and some initial boolean value
+    Given a `flag` name and some initial `boolean` value
     When I call the `add` method from `FlagShipService`
-    Then I'm expected a new flag added.
+    Then I'm expected a new `Flag` instance to be return
+         with the right arguments.
     """
 
     # Given
-    flag_ship = FlagShipService()
-    flag_name_ = "my first flag"
+    flagship = FlagShipService()
+    flag_name = "my first flag"
     value = False
+
     # When
-    assert flag_ship.add(name=flag_name_, value=value) == "ok"
+    expected_flag = flagship.add(name=flag_name, value=value)
+
     # Then
+    assert (
+        expected_flag.name == flag_name
+    ), "Store flag name should be the same as the input"
+    assert (
+        expected_flag.value == value
+    ), "Store flag value should  be the same as the input"
+    assert isinstance(
+        expected_flag.id, UUID
+    ), "Each flash should be associated with one uuid"
+
+
+def test_user_can_see_all_flags():
+    """
+    Given some `Flags`
+    When I call the `list` method from `FlagShipService`
+    Then I'm expecting an Iterable of flags
+    """
+    # Given
+    flagship = FlagShipService()
+    flagship.add("first flag", value=True)
+    flagship.add("second flag", value=False)
+    # When
+    flags_iter = flagship.list()
+    # Then
+    flags = list(flags_iter)
+    assert len(flags) == 2, "We should have only two flags"
